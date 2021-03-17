a901e0e505e55778bf7ef13ed18d26c4f7eb6923
Tr Th Minh Tân <mita37688>
2021-03-17 21:16:14 +0700

    3/17/2021

---
+++ Bai1_Tuan1/app/src/main/java/com/example/bai1_tuan1/bai1b.java
@@ -0,0 +1,14 @@
+package com.example.bai1_tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bai1b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai1b);
+    }
+}
